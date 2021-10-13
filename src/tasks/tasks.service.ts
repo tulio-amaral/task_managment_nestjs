f752f790c3dbc0b14447a8b47e33e1c1041a411f
tulio-amaral <tuliosantos@gee.inatel.br>
2021-10-13 17:45:12 -0300

    add validation to task service

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import Task, { TaskStatus } from './task.model';
 import { v4 as uuid } from 'uuid';
 import { CreateTaskDTO } from './dto/create-task.dto';
@@ -34,7 +34,13 @@ export class TasksService {
   }
 
   getTaskByID(id: string): Task {
-    return this.tasks.find((task) => task.id === id);
+    const task = this.tasks.find((task) => task.id === id);
+
+    if (!task) {
+      throw new NotFoundException(`Task with ID ${id} not found`);
+    }
+
+    return task;
   }
 
   createTask(createTask: CreateTaskDTO): Task {
@@ -53,7 +59,9 @@ export class TasksService {
   }
 
   deleteTask(id: string): void {
-    this.tasks = this.tasks.filter((task) => task.id !== id);
+    const taskFound = this.getTaskByID(id);
+
+    this.tasks = this.tasks.filter((task) => task.id !== taskFound.id);
   }
 
   updateTask(id: string, status: TaskStatus): Task {
