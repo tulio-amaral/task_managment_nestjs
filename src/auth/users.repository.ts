f36e7f784b70e9a82bd44f644e2b2883f3fa8049
tulio-amaral <tuliosantos@gee.inatel.br>
2021-10-14 15:35:07 -0300

    add hashed password to user signup

--- src/auth/users.repository.ts
+++ src/auth/users.repository.ts
@@ -3,6 +3,8 @@ import {
   InternalServerErrorException,
 } from '@nestjs/common';
 import { EntityRepository, Repository } from 'typeorm';
+import * as bcrypt from 'bcrypt';
+
 import { AuthCredentialsDTO } from './dto/auth-credentials.dto';
 import { User } from './user.entity';
 
@@ -13,9 +15,13 @@ export class UsersRepository extends Repository<User> {
   async createUser(userData: AuthCredentialsDTO): Promise<void> {
     const { username, password } = userData;
 
+    const salt = await bcrypt.genSalt();
+
+    const hashedPassword = await bcrypt.hash(password, salt);
+
     const user = this.create({
       username,
-      password,
+      password: hashedPassword,
     });
 
     try {
