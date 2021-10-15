795ee3874a6c96005199ef673aa7581b504c3f9c
tulio-amaral <tuliosantos@gee.inatel.br>
2021-10-15 10:47:41 -0300

    create relations between user and tasks

--- src/tasks/task.entity.ts
+++ src/tasks/task.entity.ts
@@ -1,4 +1,5 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { User } from 'src/auth/user.entity';
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
 
 import { TaskStatus } from './task-status.enum';
 
@@ -15,4 +16,7 @@ export class Task {
 
   @Column()
   status: TaskStatus;
+
+  @ManyToOne((_type) => User, (user) => user.tasks, { eager: false })
+  user: User;
 }
